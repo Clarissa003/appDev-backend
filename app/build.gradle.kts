a505bf4275973abf9ae9abef3baf8d8c74772f88
Alicia-Rodriguez1 <111961581+Alicia-Rodriguez1@users.noreply.github.com>
2024-05-10 16:12:25 +0200

    Edit: add collapsible calendar library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation ("com.github.shrikanth7698:Collapsible-Calendar-View-Android:v1.0.0")
 }
