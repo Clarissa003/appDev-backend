050c04bd330d14c66f17e8ae5a67bbff8760e8fe
Clarissa003 <111962035+Clarissa003@users.noreply.github.com>
2024-05-30 21:09:41 +0200

    Add: menu logic implemented, present in unguided journal

---
+++ app/src/main/java/com/appdev/eudemonia/BaseActivity.kt
@@ -0,0 +1,53 @@
+package com.appdev.eudemonia
+
+import com.appdev.eudemonia.R
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        when (item.itemId) {
+            R.id.profile -> {
+                // Navigate to Profile page
+                startActivity(Intent(this, ProfileActivity::class.java))
+                return true
+            }
+
+            R.id.moods -> {
+                // Navigate to Moods page
+                startActivity(Intent(this, MoodsActivity::class.java))
+                return true
+            }
+
+            R.id.habit -> {
+                // Navigate to Habit page
+                startActivity(Intent(this, HabitsActivity::class.java))
+                return true
+            }
+
+            R.id.unguidedJournal -> {
+                // Navigate to Unguided Journal page
+                startActivity(Intent(this, UnguidedJournalActivity::class.java))
+                return true
+            }
+
+            R.id.guidedJournal -> {
+                // Navigate to Guided Journal page
+                startActivity(Intent(this, GuidedJournalActivity::class.java))
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
