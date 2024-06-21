9ad8a7758ad658e434ccbc6bb9c238b013b36c16
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2024-06-21 14:25:33 +0200

    fix: splash screen

--- app/src/main/java/com/appdev/eudemonia/SplashScreenActivity.kt
+++ app/src/main/java/com/appdev/eudemonia/SplashScreenActivity.kt
@@ -1,20 +1,38 @@
 package com.appdev.eudemonia
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val splashImageView: View = findViewById(R.id.iv_splash)
+
+        // Create an alpha animation
+        val alphaAnimation = AlphaAnimation(0.0f, 1.0f).apply {
+            duration = 2000 // duration in milliseconds
+            fillAfter = true // keep the final state after the animation
         }
+
+        // Start the animation
+        splashImageView.startAnimation(alphaAnimation)
+
+        // Duration of wait (total time for splash screen to be visible)
+        val splashScreenDuration = 3000L // 3 seconds
+
+        // Handler to start the main activity and close this splash activity after some seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start main activity
+            startActivity(Intent(this, SignupActivity::class.java))
+            // Close splash activity
+            finish()
+        }, splashScreenDuration)
     }
 }
