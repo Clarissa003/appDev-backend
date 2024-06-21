060cccceb35121e694da01b95d3bb33eb37a6576
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2024-06-21 02:01:36 +0200

    fix: gradle merge conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,16 +68,10 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation(libs.firebase.storage.ktx)
-<<<<<<< HEAD
     implementation(libs.androidx.work.runtime.ktx)
-=======
     implementation(libs.material)
-<<<<<<< HEAD
     implementation(libs.firebase.database.ktx)
-=======
     implementation(libs.androidx.activity)
->>>>>>> origin/dev
->>>>>>> origin/dev
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.auth.ktx)
@@ -85,9 +79,7 @@ dependencies {
     implementation(libs.androidx.media3.common)
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-<<<<<<< HEAD
     implementation ("com.squareup.picasso:picasso:2.71828")
-=======
     implementation ("com.google.android.material:material:1.4.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -97,7 +89,6 @@ dependencies {
     implementation ("androidx.media3:media3-ui:1.0.0")
     implementation ("androidx.media3:media3-session:1.0.0")
 
->>>>>>> origin/dev
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
