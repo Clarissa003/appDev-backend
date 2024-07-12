2af3bb9d8fc470f53f27d95eaf41c4f8f8b7e8da
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2024-07-12 15:55:25 +0200

    fix: still trying to fix the redirect

--- app/src/main/java/com/appdev/eudemonia/menu/BaseActivity.kt
+++ app/src/main/java/com/appdev/eudemonia/menu/BaseActivity.kt
@@ -21,6 +21,33 @@ open class BaseActivity : AppCompatActivity() {
         Log.d("BaseActivity", "BottomNavigationView: $bottomNavigationView")
 
         bottomNavigationView.setupWithNavController(navController)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            Log.d("BaseActivity", "Menu item selected: ${item.title}")
+            val handled = when (item.itemId) {
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.navigation_friends -> {
+                    navController.navigate(R.id.navigation_friends)
+                    true
+                }
+                R.id.navigation_profile -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                R.id.navigation_journals -> {
+                    navController.navigate(R.id.journalFragment)
+                    true
+                }
+                R.id.navigation_songs -> {
+                    navController.navigate(R.id.soundsFragment)
+                    true
+                }
+                else -> false
+            }
+            handled
+        }
     }
 }
-
